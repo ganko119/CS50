72e78a549240fef2bff56b1bb745e7a73afcb4cb
ganko119 <84271659+ganko119@users.noreply.github.com>
2021-08-22 15:22:06 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+//Run script once DOM is loaded
+document.addEventListener('DOMContentLoaded', function(){
+
+    //Mr.Kambe's big project
+    let pj = document.querySelector('#dazaifu');
+    pj.addEventListener('click', function(){
+        let limage = window.open("https://www.dazaifutenmangu.or.jp/en/", "width=488, height=368, status=1, menubar=1");
+     });
+});
